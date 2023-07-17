97261db9c1bb1a8423a7846158e7a4155bf3e1dd
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-17 12:19:51 +0600

    Initial Commit

---
+++ AsyncAwaitIOS/AsyncAwaitIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitIOSApp.swift
+//  AsyncAwaitIOS
+//
+//  Created by Rezaul Islam on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
